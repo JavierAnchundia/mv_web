1c2cf90c178704c6c301de75caaab38e812fd130
Carla Sánchez <carmsanc@espol.edu.ec>
2020-12-07 11:22:21 -0500

    [#173932689] Se modificaron las vistas del home y de paquetes para mostrar paquetes desde backend

--- MVWeb/src/app/pages/home/home/home.component.ts
+++ MVWeb/src/app/pages/home/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SlickCarouselComponent } from 'ngx-slick-carousel';
+import URL_SERVICIOS from 'src/app/config/config';
+import { PaquetesService } from '../../../services/paquete/paquetes.service';
 
 @Component({
   selector: 'app-home',
@@ -8,26 +10,53 @@ import { SlickCarouselComponent } from 'ngx-slick-carousel';
 })
 export class HomeComponent implements OnInit {
   @ViewChild('slickModal') slickModal: SlickCarouselComponent;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
   slides = [342, 453, 846, 855, 234, 564, 744, 243];
+  public paquetes = [];
+  url_backend: String = URL_SERVICIOS.url_backend;
 
   slideConfig = {
-    "slidesToShow": 3,
-    "slidesToScroll": 1,
-    "nextArrow": "<div class='nav-btn next-slide'></div>",
-    "prevArrow": "<div class='nav-btn prev-slide'></div>",
-    "dots": true,
-    "infinite": false,
-    "arrows": true
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: '<div class=\'nav-btn next-slide\'></div>',
+    prevArrow: '<div class=\'nav-btn prev-slide\'></div>',
+    dots: true,
+    infinite: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    variableHeight: true,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   };
 
+  constructor(
+    private paquete: PaquetesService
+  ) { }
+
+  ngOnInit(): void {
+    this.cargarPaquetes();
+  }
+
   addSlide() {
-    this.slides.push(488)
+    this.slides.push(488);
   }
 
   removeSlide() {
@@ -53,9 +82,18 @@ export class HomeComponent implements OnInit {
   next() {
     this.slickModal.slickNext();
   }
-  
+
   prev() {
     this.slickModal.slickPrev();
+  }
+
+  cargarPaquetes() {
+    const id = JSON.parse(localStorage.getItem('info'));
+    this.paquete.getPaquetes(id.camposanto).subscribe(
+      (data: any) => {
+        this.paquetes = data;
+      }
+    );
   }
 
 }
---
+++ MVWeb/src/app/pages/paquetes/paquetes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import URL_SERVICIOS from 'src/app/config/config';
+import { PaquetesService } from 'src/app/services/paquete/paquetes.service';
+
+@Component({
+  selector: 'app-paquetes',
+  templateUrl: './paquetes.component.html',
+  styleUrls: ['./paquetes.component.css']
+})
+export class PaquetesComponent implements OnInit {
+  public paquetes = [];
+  url_backend = URL_SERVICIOS.url_backend;
+
+  constructor(
+    private paquete: PaquetesService,
+  ) { }
+
+  ngOnInit(): void {
+    this.cargarPaquetes();
+  }
+
+  cargarPaquetes(){
+    const id = JSON.parse(localStorage.getItem('info'));
+    this.paquete.getPaquetes(id.camposanto).subscribe(
+      (data: any) => {
+        this.paquetes = data;
+      }
+    );
+  }
+}
